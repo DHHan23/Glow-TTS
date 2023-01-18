56095e5726635dbd45d133120ab6123e63621015
DHHan23 <122949209+DHHan23@users.noreply.github.com>
2023-01-18 17:38:37 +0900

    train.py epoch 넘버비교해서 삭제하는 로직 추가

--- train.py
+++ train.py
@@ -88,6 +88,11 @@ def train_and_eval(rank, n_gpus, hps):
       train(rank, epoch, hps, generator, optimizer_g, train_loader, logger, writer)
       evaluate(rank, epoch, hps, generator, optimizer_g, val_loader, logger, writer_eval)
       utils.save_checkpoint(generator, optimizer_g, hps.train.learning_rate, epoch, os.path.join(hps.model_dir, "G_{}.pth".format(epoch)))
+      # HDH 추가 
+      logger.info("HDH-크기비교={}".format(epoch-10 > 3552))
+      if epoch-10 > 3552:
+        for index in range(3553,epoch-10):
+          utils.delete_checkpoint_fromGoogleDrive(index, os.path.join(hps.model_dir, "G_{}.pth".format(index)))
     else:
       train(rank, epoch, hps, generator, optimizer_g, train_loader, None, None)
 
